2edb0fb6b5cd7361646fa45db19ea05401271ec0
LaviniBastos <bastoslavini@gmail.com>
2025-03-06 13:07:25 -0300

    PythonEscoladaNuvem

---
+++ Atividade4.py
@@ -0,0 +1,9 @@
+# Este código calcula e mostra em tela o preço total dos produtos comprados com base na quantidade comprada
+
+produto = "cadeira infantil"
+preco_unitario = 12.40
+quant = 3
+
+preco_total = preco_unitario * quant
+
+print(f"O produto {produto} de valor R$ {preco_unitario:.2f}, foi comprado {quant} vezes, resultando em um total de: {preco_total:.2f}")
